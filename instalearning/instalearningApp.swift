d2a0c83bf2cbfbdeba90ecffea63a3499b6cb91f
torresrodrigo <33295900+torresrodrigo@users.noreply.github.com>
2025-08-29 16:54:38 -0300

    Initial Commit

---
+++ instalearning/instalearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  instalearningApp.swift
+//  instalearning
+//
+//  Created by Rodrigo Torres on 29/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct instalearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
